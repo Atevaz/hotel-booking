cb3778997894a8c7cd023a3a6396a8845ecca638
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2022-09-20 20:41:42 +0200

    created initial profile screen

---
+++ lib/presentation/screens/shared/profile_layout/profile_screen.dart
@@ -0,0 +1,72 @@
+import 'package:booking_hotel/presentation/widget/medium_text.dart';
+import 'package:booking_hotel/presentation/widget/profile_screen_component.dart';
+import 'package:booking_hotel/presentation/widget/text_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        ListTile(
+          title: const MediumText(text: 'Mohamed'),
+          subtitle: DefaultTextButton(
+            text: 'View and Edit profile',
+            onPressed: () {},
+          ),
+          trailing: const CircleAvatar(
+            radius: 40,
+            backgroundImage: AssetImage('assets/user.jpg'),
+          ),
+        ),
+        SizedBox(
+          height: 30.h,
+        ),
+        const ProfileScreenComponent(
+            componentTitle: 'Change Password',
+            componentIcon: Icon(
+              Icons.lock,
+              size: 30,
+            )),
+        const Spacer(),
+        const ProfileScreenComponent(
+            componentTitle: 'Invite Friend',
+            componentIcon: Icon(
+              Icons.people,
+              size: 30,
+            )),
+        const Spacer(),
+        const ProfileScreenComponent(
+            componentTitle: 'Credit & Coupons',
+            componentIcon: Icon(
+              Icons.card_giftcard,
+              size: 30,
+            )),
+        const Spacer(),
+        const ProfileScreenComponent(
+            componentTitle: 'Help Center',
+            componentIcon: Icon(
+              Icons.help,
+              size: 30,
+            )),
+        const Spacer(),
+        const ProfileScreenComponent(
+            componentTitle: 'Payment',
+            componentIcon: Icon(
+              Icons.wallet,
+              size: 30,
+            )),
+        const Spacer(),
+        const ProfileScreenComponent(
+            componentTitle: 'Settings',
+            componentIcon: Icon(
+              Icons.settings,
+              size: 30,
+            )),
+      ]),
+    );
+  }
+}

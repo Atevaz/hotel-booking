fc92e59c365ec797626228187d01c444c1ad0b93
Atevaz <101355785+Atevaz@users.noreply.github.com>
2022-09-20 13:12:07 +0200

    app bar bottom nav bar custom button text button text form field

--- lib/presentation/view/my_app_bar.dart
+++ lib/presentation/view/my_app_bar.dart
@@ -1,36 +1,51 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatelessWidget {
-  final String title;
-  final Widget? leadingIcon;
 
-  //Widget? actionIcon;
-  final List<Widget>? actions;
-  final bool isBack;
+  final bool isCompletedAppBar;
+  Widget leadingIcon ;
+  List<Widget>? actions;
+  String? title;
+  bool? isBack;
 
-  const MyAppBar({
+   MyAppBar({
     Key? key,
-    this.isBack = true,
-    this.title = '',
-    this.leadingIcon,
-    //this.actionIcon,
-    this.actions,
+     this.isCompletedAppBar = true,
+     this.title,
+     this.isBack,
+     this.leadingIcon = const Icon(Icons.arrow_back_ios),
+     this.actions
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-          onPressed: () {
-            if (isBack == true) {
-              Navigator.pop(context);
-            }
-          },
-          icon: leadingIcon!),
-      title: Text(title),
-      actions: actions,
-      centerTitle: false,
-      // backgroundColor: AppColor.black,
-    );
+
+    if(isCompletedAppBar){
+      return AppBar(
+        leading: IconButton(
+            onPressed: () {
+              if (isBack == true) {
+                Navigator.pop(context);
+              }
+            },
+            icon: leadingIcon),
+        title: title != null ? Text(title!) : const Text(''),
+        actions: actions,
+        centerTitle: false,
+        // backgroundColor: AppColor.black,
+      );
+    }
+    else{
+      return AppBar(
+        leading: IconButton(
+            onPressed: () {
+              if (isBack == true) {
+                Navigator.pop(context);
+              }
+            },
+            icon: leadingIcon),
+      );
+    }
+
   }
 }

b886987c3b38f2c0fb5716f3ef53dce208ecd2bb
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2022-09-21 16:35:05 +0200

    edited profile screen

--- lib/presentation/screens/user/profile_layout/profile_screen.dart
+++ lib/presentation/screens/user/profile_layout/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:booking_hotel/core/router/app_router_names.dart';
+import 'package:booking_hotel/core/styles/colors.dart';
+import 'package:booking_hotel/presentation/widget/headline_text.dart';
 import 'package:booking_hotel/presentation/widget/medium_text.dart';
 import 'package:booking_hotel/presentation/widget/profile_screen_component.dart';
 import 'package:booking_hotel/presentation/widget/text_button.dart';
@@ -11,66 +13,78 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        ListTile(
-          title: const MediumText(text: 'Mohamed'),
-          subtitle: DefaultTextButton(
-            text: 'View and Edit profile',
-            onPressed: () {
-              Navigator.pushNamed(
-                  context, AppRouterNames.rUpdateProfileLayoutRoute);
-            },
-          ),
-          trailing: const CircleAvatar(
-            radius: 40,
-            backgroundImage: AssetImage('assets/user.jpg'),
-          ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(children: [
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const HeadLineText(text: ' Mohamed'),
+                    DefaultTextButton(
+                      text: 'View and Edit profile',
+                      color: AppColor.grey,
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, AppRouterNames.rUpdateProfileLayoutRoute);
+                      },
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                const CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('assets/images/user.jpg'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h),
+            const ProfileScreenComponent(
+                componentTitle: 'Change Password',
+                componentIcon: Icon(
+                  Icons.lock,
+                  size: 30,
+                )),
+            const Divider(color: AppColor.grey),
+            const ProfileScreenComponent(
+                componentTitle: 'Invite Friend',
+                componentIcon: Icon(
+                  Icons.people,
+                  size: 30,
+                )),
+            const Divider(color: AppColor.grey),
+            const ProfileScreenComponent(
+                componentTitle: 'Credit & Coupons',
+                componentIcon: Icon(
+                  Icons.card_giftcard,
+                  size: 30,
+                )),
+            const Divider(color: AppColor.grey),
+            const ProfileScreenComponent(
+                componentTitle: 'Help Center',
+                componentIcon: Icon(
+                  Icons.help,
+                  size: 30,
+                )),
+            const Divider(color: AppColor.grey),
+            const ProfileScreenComponent(
+                componentTitle: 'Payment',
+                componentIcon: Icon(
+                  Icons.wallet,
+                  size: 30,
+                )),
+            const Divider(color: AppColor.grey),
+            const ProfileScreenComponent(
+                componentTitle: 'Settings',
+                componentIcon: Icon(
+                  Icons.settings,
+                  size: 30,
+                )),
+          ]),
         ),
-        SizedBox(
-          height: 30.h,
-        ),
-        const ProfileScreenComponent(
-            componentTitle: 'Change Password',
-            componentIcon: Icon(
-              Icons.lock,
-              size: 30,
-            )),
-        const Spacer(),
-        const ProfileScreenComponent(
-            componentTitle: 'Invite Friend',
-            componentIcon: Icon(
-              Icons.people,
-              size: 30,
-            )),
-        const Spacer(),
-        const ProfileScreenComponent(
-            componentTitle: 'Credit & Coupons',
-            componentIcon: Icon(
-              Icons.card_giftcard,
-              size: 30,
-            )),
-        const Spacer(),
-        const ProfileScreenComponent(
-            componentTitle: 'Help Center',
-            componentIcon: Icon(
-              Icons.help,
-              size: 30,
-            )),
-        const Spacer(),
-        const ProfileScreenComponent(
-            componentTitle: 'Payment',
-            componentIcon: Icon(
-              Icons.wallet,
-              size: 30,
-            )),
-        const Spacer(),
-        const ProfileScreenComponent(
-            componentTitle: 'Settings',
-            componentIcon: Icon(
-              Icons.settings,
-              size: 30,
-            )),
-      ]),
+      ),
     );
   }
 }

55664f9c6f5c9c3a2eab1f497eacc6151103d33c
belalemad107 <69440633+belalemad107@users.noreply.github.com>
2022-09-26 15:32:28 +0200

    update profile - handeled

--- lib/data/repository/profile/profile_repository_impl.dart
+++ lib/data/repository/profile/profile_repository_impl.dart
@@ -82,14 +82,17 @@ class ProfileRepositoryImpl implements ProfileRepository {
     try {
       final isConnected = await networkService.isConnected;
       if (!isConnected) {
-        throw const NetworkException(message: "No Internet Connection!");
+        throw const NetworkException(
+          arMessage: Network_Connection_Ar_Error,
+          enMessage: Network_Connection_En_Error,
+        );
       }
       final result = await profileDataSource.updateProfile(name, email, image);
       return Right(result);
     } on NetworkException catch (e) {
-      return Left(e.message);
+      return Left(isEng ? e.enMessage : e.arMessage);
     } on ServerException catch (e) {
-      return Left(e.message);
+      return Left(isEng ? e.enMessage : e.arMessage);
     } catch (e) {
       return Left("$e");
     }
